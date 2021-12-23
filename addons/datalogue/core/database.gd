6517f80d3fc4da1849a55599a99f397653b64c57
BraindeadBZH <braindeadbzh@gmail.com>
2021-12-23 12:16:00 +0100

    Added database statistics.

--- addons/datalogue/core/database.gd
+++ addons/datalogue/core/database.gd
@@ -8,10 +8,12 @@ signal changed()
 
 var _id := ""
 var _items := {}
+var _statistics := {}
 
 
 func _init(id: String):
 	_id = id
+	_init_stats()
 
 
 func id() -> String:
@@ -38,6 +40,10 @@ func count() -> int:
 	return _items.size()
 
 
+func statistics() -> Dictionary:
+	return _statistics
+
+
 func items() -> Dictionary:
 	return _items
 
@@ -57,12 +63,14 @@ func validate_item_id(id: String) -> String:
 
 func add_item(item: DlItem) -> void:
 	_items[item.id()] = item
+	_update_stats()
 	emit_signal("changed")
 
 
 func copy_item(origin: DlItem, new_id: String) -> void:
 	var item := origin.duplicate(new_id)
 	_items[new_id] = item
+	_update_stats()
 	emit_signal("changed")
 
 
@@ -79,6 +87,7 @@ func get_item(id: String) -> DlItem:
 
 func update_item(modified_item: DlItem, old_id: String = "") -> void:
 	_items[modified_item.id()] = modified_item
+	_update_stats()
 	emit_signal("changed")
 	
 	if not old_id.is_empty():
@@ -87,4 +96,63 @@ func update_item(modified_item: DlItem, old_id: String = "") -> void:
 
 func remove_item(id: String) -> void:
 	_items.erase(id)
+	_update_stats()
 	emit_signal("changed")
+
+
+func _init_stats() -> void:
+	_statistics.clear()
+	
+	_statistics = {
+		# <class id> -> <items with class>
+		"classes": {},
+		# <class id> -> <class value> -> <items with class value>
+		"classes_values": {},
+		# <value id> -> <items with value>
+		"values": {},
+		# <value id> -> <minimum>
+		"values_min": {},
+		# <value id> -> <maximum>
+		"values_max": {},
+		# <text id> -> <items with text>
+		"texts": {}
+	}
+
+
+func _update_stats() -> void:
+	_init_stats()
+	
+	for id in _items:
+		var item := _items[id] as DlItem
+		for classif_id in item.classifications():
+			if _statistics["classes"].has(classif_id):
+				_statistics["classes"][classif_id] += 1
+			else:
+				_statistics["classes"][classif_id] = 1
+				_statistics["classes_values"][classif_id] = {}
+			
+			var classif := item.get_classification(classif_id)
+			for val_id in classif:
+				if _statistics["classes_values"][classif_id].has(val_id):
+					_statistics["classes_values"][classif_id][val_id] += 1
+				else:
+					_statistics["classes_values"][classif_id][val_id] = 1
+		
+		for val_id in item.values():
+			var val := item.get_value(val_id)
+			if _statistics["values"].has(val_id):
+				_statistics["values"][val_id] += 1
+				if val < _statistics["values_min"][val_id]:
+					_statistics["values_min"][val_id] = val
+				if val > _statistics["values_max"][val_id]:
+					_statistics["values_max"][val_id] = val
+			else:
+				_statistics["values"][val_id] = 1
+				_statistics["values_min"][val_id] = val
+				_statistics["values_max"][val_id] = val
+		
+		for txt_id in item.texts():
+			if _statistics["texts"].has(txt_id):
+				_statistics["texts"][txt_id] += 1
+			else:
+				_statistics["texts"][txt_id] = 1
